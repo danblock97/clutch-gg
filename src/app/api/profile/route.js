8048f850c7546dfd3bc91dcc10e44d30eb26e096
danblock97 <generalgoldiez@gmail.com>
2024-04-25 11:45:45 +0100

    Refactor profile route to fetch account and profile data from Riot API

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,6 +1,20 @@
 import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
+const regions = [
+	"BR1",
+	"EUN1",
+	"EUW1",
+	"JP1",
+	"KR",
+	"LA1",
+	"LA2",
+	"NA1",
+	"OC1",
+	"TR1",
+	"RU",
+];
+
 export async function GET(req, res) {
 	const gameName = req.nextUrl.searchParams.get("gameName");
 	const tagLine = req.nextUrl.searchParams.get("tagLine");
@@ -9,7 +23,7 @@ export async function GET(req, res) {
 		return NextResponse.error("Missing required query parameters");
 	}
 
-	const account = await fetch(
+	const accountResponse = await fetch(
 		`https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`,
 		{
 			headers: {
@@ -19,10 +33,40 @@ export async function GET(req, res) {
 		}
 	);
 
-	if (!account.ok) {
+	if (!accountResponse.ok) {
 		return NextResponse.error("Failed to fetch profile");
 	}
 
-	const accountData = await account.json();
-	return NextResponse.json(accountData);
+	const accountData = await accountResponse.json();
+
+	const encryptedPUUID = accountData.puuid;
+
+	let profileResponse;
+	let profileData;
+	for (const region of regions) {
+		profileResponse = await fetch(
+			`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${encryptedPUUID}`,
+			{
+				headers: {
+					"X-Riot-Token": process.env.RIOT_API_KEY,
+				},
+				revalidatePath: revalidatePath(req.nextUrl.pathname),
+			}
+		);
+		if (profileResponse.ok) {
+			profileData = await profileResponse.json();
+			break;
+		}
+	}
+
+	if (!profileResponse || !profileResponse.ok) {
+		return NextResponse.error("Failed to fetch profile");
+	}
+
+	const data = {
+		profileData,
+		accountData,
+	};
+
+	return NextResponse.json(data);
 }
