85984c28656cfab757c4be9bb695ff60b2e66ef1
danblock97 <generalgoldiez@gmail.com>
2024-04-26 18:07:44 +0100

    Refactor SearchBar component to use combinedInput instead of separate gameName and tagLine states

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,11 +4,11 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
 const SearchBar = () => {
-	const [gameName, setGameName] = useState("");
-	const [tagLine, setTagLine] = useState("");
+	const [combinedInput, setCombinedInput] = useState("");
 	const router = useRouter();
 
 	const handleSearch = () => {
+		const [gameName, tagLine] = combinedInput.split("#");
 		router.push(`/profile?gameName=${gameName}&tagLine=${tagLine}`);
 	};
 
@@ -20,23 +20,12 @@ const SearchBar = () => {
 
 	return (
 		<div className="flex flex-col sm:flex-row items-center justify-center p-4 bg-gray-800 rounded-lg">
-			<div className="flex justify-center sm:justify-start mb-4 sm:mb-0 sm:mr-2">
-				<input
-					className="flex-1 p-2 mb-4 sm:mb-0 sm:mr-2 border border-gray-300 rounded bg-gray-700 text-white placeholder-gray-400"
-					type="text"
-					placeholder="gameName"
-					value={gameName}
-					onChange={(e) => setGameName(e.target.value)}
-					onKeyDown={handleKeyDown}
-				/>
-			</div>
-
 			<input
 				className="flex-1 p-2 mb-4 sm:mb-0 sm:mr-2 border border-gray-300 rounded bg-gray-700 text-white placeholder-gray-400"
 				type="text"
-				placeholder="tagLine"
-				value={tagLine}
-				onChange={(e) => setTagLine(e.target.value)}
+				placeholder="gameName#tagLine"
+				value={combinedInput}
+				onChange={(e) => setCombinedInput(e.target.value)}
 				onKeyDown={handleKeyDown}
 			/>
 
