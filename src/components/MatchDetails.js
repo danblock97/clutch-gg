918eccfeb0799ac1f8d189c013c094519db434ad
danblock97 <generalgoldiez@gmail.com>
2024-04-30 09:34:36 +0100

    Refactor MatchDetails component to include champion rune icons, summoner spell images, position, and accountData

--- src/components/MatchDetails.js
+++ src/components/MatchDetails.js
@@ -1,5 +1,88 @@
+import React from "react";
 import Image from "next/image";
 import Loading from "./Loading";
+
+const ParticipantDetails = ({ participant }) => {
+	return (
+		<div className="flex justify-between items-center p-4 my-2 rounded-lg bg-gray-800">
+			{/* Position Icon and Champion Name */}
+			<div className="flex items-center space-x-2">
+				<Image
+					className="w-8 h-8"
+					src={`https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-champ-select/global/default/svg/position-${participant.individualPosition.toLowerCase()}.svg`}
+					alt={`${participant.individualPosition} Position Icon`}
+					width={32}
+					height={32}
+				/>
+				<Image
+					className="w-8 h-8"
+					src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/${participant.championId}.png`}
+					alt="Champion"
+					width={32}
+					height={32}
+				/>
+				<span className="text-sm font-semibold">
+					{participant.riotIdGameName}#{participant.riotIdTagline}
+				</span>
+			</div>
+			{/* Rune Icon */}
+			<Image
+				className="w-8 h-8"
+				src={`/images/runeIcons/${participant.perks.styles[0].selections[0].perk}.png`}
+				alt="Champion"
+				width={32}
+				height={32}
+			/>
+			{/* Summoner Spells */}
+			<div className="flex items-center space-x-2">
+				{[participant.summoner1Id, participant.summoner2Id].map(
+					(spellId, idx) => (
+						<div key={idx} className="flex items-center">
+							<Image
+								src={`/images/summonerSpells/${spellId}.png`}
+								alt={`Summoner Spell ${idx + 1}`}
+								width={32}
+								height={32}
+							/>
+						</div>
+					)
+				)}
+			</div>
+			{/* Items */}
+			<div className="flex items-center space-x-2">
+				{Array.from({ length: 7 }, (_, i) => participant[`item${i}`]).map(
+					(itemId, idx) => (
+						<div key={idx} className="flex items-center">
+							{itemId > 0 ? (
+								<Image
+									className="w-8 h-8"
+									src={`https://ddragon.leagueoflegends.com/cdn/14.8.1/img/item/${itemId}.png`}
+									alt="Item"
+									width={32}
+									height={32}
+								/>
+							) : (
+								<Image
+									className="w-8 h-8"
+									src="/images/placeholder.png"
+									alt="No item"
+									width={32}
+									height={32}
+								/>
+							)}
+						</div>
+					)
+				)}
+			</div>
+			{/* Stats */}
+			<div className="flex flex-wrap items-center space-x-2">
+				<span className="text-xs font-semibold">{`${participant.kills} / ${participant.deaths} / ${participant.assists}`}</span>
+				<span className="text-xs font-semibold">{`${participant.totalMinionsKilled} CS`}</span>
+				<span className="text-xs font-semibold">{`${participant.totalDamageDealtToChampions.toLocaleString()} DMG`}</span>
+			</div>
+		</div>
+	);
+};
 
 const MatchDetails = ({ matchDetails, matchId, accountData }) => {
 	if (!matchDetails) {
@@ -18,7 +101,6 @@ const MatchDetails = ({ matchDetails, matchId, accountData }) => {
 		);
 	}
 
-	// Splitting participants into two teams
 	const team1 = match.info.participants.filter(
 		(participant) => participant.teamId === 100
 	);
@@ -35,92 +117,7 @@ const MatchDetails = ({ matchDetails, matchId, accountData }) => {
 						Team 1
 					</span>
 					{team1.map((participant, index) => (
-						<div
-							key={index}
-							className={`flex justify-between items-center p-4 my-2 rounded-lg ${
-								index % 2 === 0 ? "bg-gray-800" : "bg-gray-700"
-							}`}
-						>
-							{/* Position Icon and Champion Name */}
-							<div className="flex items-center space-x-2">
-								{/* Position Icon */}
-								<Image
-									className="w-8 h-8"
-									src={`https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-champ-select/global/default/svg/position-${participant.individualPosition.toLowerCase()}.svg`}
-									alt={`${participant.individualPosition} Position Icon`}
-									width={32}
-									height={32}
-								/>
-								{/* Champion Icon */}
-								<Image
-									className="w-8 h-8"
-									src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/${participant.championId}.png`}
-									alt="Champion"
-									width={32}
-									height={32}
-								/>
-								{/* Champion Name */}
-								<span className="text-sm font-semibold">
-									{participant.riotIdGameName}#{participant.riotIdTagline}
-								</span>
-							</div>
-							<Image
-								className="w-8 h-8"
-								src={`/images/runeIcons/${participant.perks.styles[0].selections[0].perk}.png`}
-								alt="Champion"
-								width={32}
-								height={32}
-							/>
-							<div className="flex items-center space-x-2">
-								{/* Summoner Spells */}
-								{[participant.summoner1Id, participant.summoner2Id].map(
-									(spellId, idx) => (
-										<div key={idx} className="flex items-center">
-											<Image
-												src={`/images/summonerSpells/${spellId}.png`}
-												alt={`Summoner Spell ${idx + 1}`}
-												width={32}
-												height={32}
-											/>
-										</div>
-									)
-								)}
-							</div>
-							{/* Spells, Items, KDA, CS, Damage */}
-							<div className="flex items-center space-x-2">
-								{/* Items */}
-								{Array.from(
-									{ length: 7 },
-									(_, i) => participant[`item${i}`]
-								).map((itemId, idx) => (
-									<div key={idx} className="flex items-center">
-										{itemId > 0 ? (
-											<Image
-												className="w-8 h-8"
-												src={`https://ddragon.leagueoflegends.com/cdn/14.8.1/img/item/${itemId}.png`}
-												alt="Item"
-												width={32}
-												height={32}
-											/>
-										) : (
-											<Image
-												className="w-8 h-8"
-												src="/images/placeholder.png" // Replace this with the actual path to your placeholder image
-												alt="No item"
-												width={32}
-												height={32}
-											/>
-										)}
-									</div>
-								))}
-								{/* Stats */}
-							</div>
-							<div className="flex flex-wrap items-center space-x-2">
-								<span className="text-xs font-semibold">{`${participant.kills} / ${participant.deaths} / ${participant.assists}`}</span>
-								<span className="text-xs font-semibold">{`${participant.totalMinionsKilled} CS`}</span>
-								<span className="text-xs font-semibold">{`${participant.totalDamageDealtToChampions.toLocaleString()} DMG`}</span>
-							</div>
-						</div>
+						<ParticipantDetails key={index} participant={participant} />
 					))}
 				</div>
 				{/* Team 2 */}
@@ -129,92 +126,7 @@ const MatchDetails = ({ matchDetails, matchId, accountData }) => {
 						Team 2
 					</span>
 					{team2.map((participant, index) => (
-						<div
-							key={index}
-							className={`flex justify-between items-center p-4 my-2 rounded-lg ${
-								index % 2 === 0 ? "bg-gray-800" : "bg-gray-700"
-							}`}
-						>
-							{/* Position Icon and Champion Name */}
-							<div className="flex items-center space-x-2">
-								{/* Position Icon */}
-								<Image
-									className="w-8 h-8"
-									src={`https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-champ-select/global/default/svg/position-${participant.individualPosition.toLowerCase()}.svg`}
-									alt={`${participant.individualPosition} Position Icon`}
-									width={32}
-									height={32}
-								/>
-								{/* Champion Icon */}
-								<Image
-									className="w-8 h-8"
-									src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/${participant.championId}.png`}
-									alt="Champion"
-									width={32}
-									height={32}
-								/>
-								{/* Champion Name */}
-								<span className="text-sm font-semibold">
-									{participant.riotIdGameName}#{participant.riotIdTagline}
-								</span>
-							</div>
-							<Image
-								className="w-8 h-8"
-								src={`/images/runeIcons/${participant.perks.styles[0].selections[0].perk}.png`}
-								alt="Champion"
-								width={32}
-								height={32}
-							/>
-							<div className="flex items-center space-x-2">
-								{/* Summoner Spells */}
-								{[participant.summoner1Id, participant.summoner2Id].map(
-									(spellId, idx) => (
-										<div key={idx} className="flex items-center">
-											<Image
-												src={`/images/summonerSpells/${spellId}.png`}
-												alt={`Summoner Spell ${idx + 1}`}
-												width={32}
-												height={32}
-											/>
-										</div>
-									)
-								)}
-							</div>
-							{/* Spells, Items, KDA, CS, Damage */}
-							<div className="flex items-center space-x-2">
-								{/* Items */}
-								{Array.from(
-									{ length: 7 },
-									(_, i) => participant[`item${i}`]
-								).map((itemId, idx) => (
-									<div key={idx} className="flex items-center">
-										{itemId > 0 ? (
-											<Image
-												className="w-8 h-8"
-												src={`https://ddragon.leagueoflegends.com/cdn/14.8.1/img/item/${itemId}.png`}
-												alt="Item"
-												width={32}
-												height={32}
-											/>
-										) : (
-											<Image
-												className="w-8 h-8"
-												src="/images/placeholder.png" // Replace this with the actual path to your placeholder image
-												alt="No item"
-												width={32}
-												height={32}
-											/>
-										)}
-									</div>
-								))}
-								{/* Stats */}
-							</div>
-							<div className="flex flex-wrap items-center space-x-2">
-								<span className="text-xs font-semibold">{`${participant.kills} / ${participant.deaths} / ${participant.assists}`}</span>
-								<span className="text-xs font-semibold">{`${participant.totalMinionsKilled} CS`}</span>
-								<span className="text-xs font-semibold">{`${participant.totalDamageDealtToChampions.toLocaleString()} DMG`}</span>
-							</div>
-						</div>
+						<ParticipantDetails key={index} participant={participant} />
 					))}
 				</div>
 			</div>
