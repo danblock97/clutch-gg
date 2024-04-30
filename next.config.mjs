c0290344262b2b33237975006b2153073af0af71
danblock97 <generalgoldiez@gmail.com>
2024-04-30 11:05:37 +0100

    Add experimental flag for missingSuspenseWithCSRBailout in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
 	images: {
 		domains: ["raw.communitydragon.org", "ddragon.leagueoflegends.com"],
 	},
+	experimental: {
+		missingSuspenseWithCSRBailout: false,
+	},
 };
 
 export default nextConfig;
