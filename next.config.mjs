3f544930cb71c771c84b8fdbf5130e24032c9952
danblock97 <generalgoldiez@gmail.com>
2024-05-21 16:50:59 +0100

    Fix Build Errors

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,20 @@ const nextConfig = {
 	experimental: {
 		missingSuspenseWithCSRBailout: false,
 	},
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.resolve.fallback = {
+				fs: false,
+				net: false,
+				tls: false,
+			};
+		}
+		return config;
+	},
+	env: {
+		MONGODB_URI: process.env.MONGODB_URI,
+		RIOT_API_KEY: process.env.RIOT_API_KEY,
+	},
 };
 
 export default nextConfig;
