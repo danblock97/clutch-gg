61bb41f83538db00abe12a2b19b61b3ffc1d32e3
danblock97 <generalgoldiez@gmail.com>
2024-06-29 13:35:17 +0100

    Component created to better show a player not being in a game and linking to profile

--- src/app/live-game/page.js
+++ src/app/live-game/page.js
@@ -1,45 +1,46 @@
-"use client";
+"use client"
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import LiveGame from "@/components/LiveGame";
 import Loading from "@/components/Loading";
 import useProfileData from "../hooks/useProfileData";
+import NoActiveGameData from "@/components/NoActiveGameData"; // Import your new component
 
 const LiveGamePage = () => {
-    const { liveGameData, isLoading, error, fetchLiveGameData } = useProfileData();
-    const searchParams = useSearchParams();
-    const gameName = searchParams.get("gameName");
-    const tagLine = searchParams.get("tagLine");
+  const { liveGameData, isLoading, error, fetchLiveGameData } = useProfileData();
+  const searchParams = useSearchParams();
+  const gameName = searchParams.get("gameName");
+  const tagLine = searchParams.get("tagLine");
 
-    useEffect(() => {
-        if (gameName && tagLine) {
-            fetchLiveGameData();
-            const interval = setInterval(() => {
-                fetchLiveGameData();
-            }, 10000); // Check for updates every 10 seconds
+  useEffect(() => {
+    if (gameName && tagLine) {
+      fetchLiveGameData();
+      const interval = setInterval(() => {
+        fetchLiveGameData();
+      }, 10000); // Check for updates every 10 seconds
 
-            return () => clearInterval(interval);
-        }
-    }, [gameName, tagLine, fetchLiveGameData]);
-
-    if (isLoading) {
-        return <Loading />;
+      return () => clearInterval(interval);
     }
+  }, [gameName, tagLine, fetchLiveGameData]);
 
-    if (error) {
-        return <p className="text-red-500">{error}</p>;
-    }
+  if (isLoading) {
+    return <Loading />;
+  }
 
-    if (!liveGameData || !liveGameData.participants) {
-        return <p>No active game data available</p>;
-    }
+  if (error) {
+    return <p className="text-red-500">{error}</p>;
+  }
 
-    return (
-        <div>
-            <LiveGame liveGameData={liveGameData} />
-        </div>
-    );
+  if (!liveGameData || !liveGameData.participants) {
+    return <NoActiveGameData gameName={gameName} tagLine={tagLine} />;
+  }
+
+  return (
+    <div>
+      <LiveGame liveGameData={liveGameData} />
+    </div>
+  );
 };
 
 export default LiveGamePage;
