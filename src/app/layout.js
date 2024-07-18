397fee1be3e2e214b8b5280554e7560e52da47e2
danblock97 <generalgoldiez@gmail.com>
2024-07-18 09:06:41 +0100

    Speed Insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className={inter.className}>
+			<SpeedInsights/>
 				<NavBar />
 				{children}
 				<Footer />
