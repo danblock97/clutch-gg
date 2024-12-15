c6e53be308f446ec3e84a03433cf64aefe59af0a
danblock97 <generalgoldiez@gmail.com>
2024-12-15 09:17:41 +0000

    Fixed Bug from Customer Feedback (CL-1)

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useRef, useEffect } from "react";
 import LoadingBar from "react-top-loading-bar";
 
@@ -17,8 +19,15 @@ const Loading = () => {
     }, []);
 
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen bg-[#0e1015] flex items-center justify-center relative">
+            {/* Top Loading Bar */}
             <LoadingBar color="#f11946" ref={loadingBarRef} />
+
+            {/* Centered Spinner */}
+            <div className="flex flex-col items-center">
+                <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-gray-200"></div>
+                <h2 className="text-white text-lg mt-4">Loading...</h2>
+            </div>
         </div>
     );
 };
