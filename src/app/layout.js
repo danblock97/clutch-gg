01209a6df58a06c8db413691088cb2c70b568220
Dan <generalgoldiez@gmail.com>
2026-01-27 16:09:41 +0000

    feat(analytics): added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,25 +4,29 @@ import StructuredData from "@/components/StructuredData";
 import PropTypes from "prop-types";
 import RootLayoutContent from "./RootLayoutContent";
 import { metadata as baseMetadata } from "./metadata";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	...baseMetadata,
-	metadataBase: new URL("https://clutchgg.lol"),
+  ...baseMetadata,
+  metadataBase: new URL("https://clutchgg.lol"),
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body className={inter.className}>
-				<StructuredData type="WebSite" />
-				<RootLayoutContent>{children}</RootLayoutContent>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <StructuredData type="WebSite" />
+        <RootLayoutContent>{children}</RootLayoutContent>
+        <Analytics />
+        <SpeedInsights />
+      </body>
+    </html>
+  );
 }
 
 RootLayout.propTypes = {
-	children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
